b277a5daaf0f4b6e45379017c13b13c9e051ef7e
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2023-02-22 19:26:06 +0600

    basic data formatting done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import "./App.css";
+import './App.css';
+import Home from './Pages/Home/Home';
+import WhichClassesAreAvailable from './Pages/WhichClassesAreAvailable/WhichClassesAreAvailable';
 
 function App() {
-    return (
-        <div className="App">
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Home />
+      <WhichClassesAreAvailable />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/data/daysAndTimeSlots.js
@@ -0,0 +1,17 @@
+export const dayOptions = [
+    { value: "Saturday", label: "Saturday" },
+    { value: "Sunday", label: "Sunday" },
+    { value: "Monday", label: "Monday" },
+    { value: "Tuesday", label: "Tuesday" },
+    { value: "Wednesday", label: "Wednesday" },
+    { value: "Thursday", label: "Thursday" },
+];
+export const timeOptions = [
+    { value: "08:00 AM-09:20 AM", label: "08:00 AM-09:20 AM" },
+    { value: "09:30 AM-10:50 AM", label: "09:30 AM-10:50 AM" },
+    { value: "11:00 AM-12:20 PM", label: "11:00 AM-12:20 PM" },
+    { value: "12:30 PM-01:50 PM", label: "12:30 PM-01:50 PM" },
+    { value: "02:00 PM-03:20 PM", label: "02:00 PM-03:20 PM" },
+    { value: "03:30 PM-4:50 PM", label: "03:30 PM-4:50 PM" },
+    { value: "05:00 PM-06:20 PM", label: "05:00 PM-06:20 PM" },
+];
