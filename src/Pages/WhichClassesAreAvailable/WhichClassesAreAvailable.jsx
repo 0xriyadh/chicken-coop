560dae25c9ffee2eaaebce4d271cb4a9c1bc731c
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2023-02-22 14:03:48 +0600

    setting up env

---
+++ src/Pages/WhichClassesAreAvailable/WhichClassesAreAvailable.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WhichClassesAreAvailable = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default WhichClassesAreAvailable;
