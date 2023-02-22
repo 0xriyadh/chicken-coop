e842a0fb79a953247be9721959bb6a70599dffdf
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2023-02-22 22:27:19 +0600

    UI update with a chicken logo :3

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import Footer from "./Components/Shared/Footer/Footer";
 import Home from "./Pages/Home/Home";
 import WhichClassesAreAvailable from "./Pages/WhichClassesAreAvailable/WhichClassesAreAvailable";
+import chickenCoopLogo from "./assets/images/logo.png";
 
 function App() {
     return (
-        <div className="App container mx-auto">
-            <div className="mb-6">
-                <h1 className="text-3xl font-bold mt-14">Chicken Coop!</h1>
-                <p className="mx-4 mt-2">
+        <div className="App container mx-auto mt-12">
+            <img className="mx-auto w-32" src={chickenCoopLogo} alt="" />
+            <div className="mb-6 mt-4">
+                <h1 className="text-4xl text-gray-800 font-bold">Chicken Coop!</h1>
+                <p className="mx-4 mt-2 text-gray-600">
                     You can find all the available classrooms of BRACU at any
                     given time, by selecting the day and time below.
                 </p>
