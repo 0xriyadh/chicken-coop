7d6bf19f549ffd0ade5b2063dbbffa50c2b0ec43
Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>
2024-10-29 21:43:13 +0600

    updated till fall 24
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -14,52 +14,52 @@ const Home = () => {
       .then((data) => setGitStars(data.stargazers_count));
   }, []);
   return (
-    <div className="flex flex-col h-screen justify-between">
-      <div>
-        <p className="my-4 bg-green-50 text-green-600 py-1.5 rounded dark:text-green-300 dark:bg-green-800">
-          Updated till Summer 2024 ✅
-        </p>
-        <img className="mx-auto w-44" src={chickenCoopLogo} alt="" />
-        <div className="mb-6 mt-4">
-          <h1 className="text-4xl text-gray-800 font-bold dark:text-gray-50">
-            Chicken Coop!
-          </h1>
-          <div className="flex items-center justify-center space-x-4">
-            <a
-              href="https://github.com/mahadihassanriyadh/chicken-coop"
-              target="_blank"
-              rel="noreferrer"
-              className="bg-gray-700 text-gray-200 inline-block my-3 px-6 py-2 rounded shadow-2xl sm:text-lg transition duration-300 hover:bg-gray-800"
-            >
-              <div className="flex items-center space-x-2">
-                <span className="text-xl text-yellow-300">★</span>{" "}
-                <span>{gitStars} GitHub Stars</span>
-              </div>
-            </a>
-            <InstallPWAButton />
-            <ToggleTheme />
-          </div>
+      <div className="flex flex-col h-screen justify-between">
+          <div>
+              <p className="my-4 bg-green-100 text-green-600 py-1.5 rounded dark:text-green-300 dark:bg-green-800">
+                  Updated till Fall 2024 ✅
+              </p>
+              <img className="mx-auto w-44" src={chickenCoopLogo} alt="" />
+              <div className="mb-6 mt-4">
+                  <h1 className="text-4xl text-gray-800 font-bold dark:text-gray-50">
+                      Chicken Coop!
+                  </h1>
+                  <div className="flex items-center justify-center space-x-4">
+                      <a
+                          href="https://github.com/mahadihassanriyadh/chicken-coop"
+                          target="_blank"
+                          rel="noreferrer"
+                          className="bg-gray-700 text-gray-200 inline-block my-3 px-6 py-2 rounded shadow-2xl sm:text-lg transition duration-300 hover:bg-gray-800"
+                      >
+                          <div className="flex items-center space-x-2">
+                              <span className="text-xl text-yellow-300">★</span>{" "}
+                              <span>{gitStars} GitHub Stars</span>
+                          </div>
+                      </a>
+                      <InstallPWAButton />
+                      <ToggleTheme />
+                  </div>
 
-          <p className="mx-4 mt-2 text-gray-600 dark:text-gray-400">
-            You can find all the available classrooms of BRACU at any given
-            time, by selecting the day and time below.
-          </p>
-          <div className="flex flex-col sm:flex-row sm:space-x-6 items-center justify-center mt-5 space-y-2 sm:space-y-0">
-            <div className="flex space-x-2 items-center">
-              <span className="w-5 h-5 border bg-gray-100 inline-block rounded dark:bg-gray-700 dark:border-gray-600"></span>
-              <p>Regular Classrooms</p>
-            </div>
-            <div className="flex space-x-2 items-center -ml-7 sm:-ml-0">
-              <span className="w-5 h-5 border bg-blue-100 inline-block rounded dark:bg-blue-800/30 dark:border-blue-600/30"></span>
-              <p>Lab Classrooms</p>
-            </div>
+                  <p className="mx-4 mt-2 text-gray-600 dark:text-gray-400">
+                      You can find all the available classrooms of BRACU at any
+                      given time, by selecting the day and time below.
+                  </p>
+                  <div className="flex flex-col sm:flex-row sm:space-x-6 items-center justify-center mt-5 space-y-2 sm:space-y-0">
+                      <div className="flex space-x-2 items-center">
+                          <span className="w-5 h-5 border bg-gray-100 inline-block rounded dark:bg-gray-700 dark:border-gray-600"></span>
+                          <p>Regular Classrooms</p>
+                      </div>
+                      <div className="flex space-x-2 items-center -ml-7 sm:-ml-0">
+                          <span className="w-5 h-5 border bg-blue-100 inline-block rounded dark:bg-blue-800/30 dark:border-blue-600/30"></span>
+                          <p>Lab Classrooms</p>
+                      </div>
+                  </div>
+              </div>
+              <AvailableClasses />
           </div>
-        </div>
-        <AvailableClasses />
+          <Footer />
+          <Analytics mode={"production"} />
       </div>
-      <Footer />
-      <Analytics mode={"production"} />
-    </div>
   );
 };
 
