e206d64c12feb0a02f76a40965908216a01f5ec8
Kazi Abdur Rahim <174271695+KaziAbdurRahim@users.noreply.github.com>
2025-02-19 23:07:07 +0600

    Update src/Components/Buttons/PrimaryButton.js
    Co-authored-by: Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>

--- src/Components/Buttons/PrimaryButton.js
+++ src/Components/Buttons/PrimaryButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const PrimaryButton = (props) => {
   return (
-    <button {...props} className={props.className + " "} >
+    <button {...props} className={props.className}>
       {props?.label}
     </button>
   );
